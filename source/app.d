5ffdaf1a1f95b96db319aa6954e1de52b5a1c603
Frastandreetto <francesco.andreetto97@gmail.com>
2022-03-23 22:05:31 +0100

    Function implemented: luminosity, averageLuminosity, normalizeImage, clumpImage + working unittest

--- source/app.d
+++ source/app.d
@@ -11,6 +11,9 @@ bool areClose(float x, float y, float epsilon=1e-5){
 	return abs(x-y) < epsilon;
 }
 
+float dClamp(float x){ //(x: float) -> float: in python? what's that
+    return x / float(1 + x);
+}
 struct color{
 	float r=0.0, g=0.0, b=0.0;
 
@@ -35,6 +38,10 @@ struct color{
 
 	bool colorAreClose(color c){
 		return areClose(r, c.r) && areClose(g, c.g) && areClose(b, c.b);
+	}
+
+	float luminosity(){
+		return (max(r, g, b) + min(r, g, b)) / 2.0;
 	}
 
 	unittest{
@@ -92,24 +99,51 @@ class HDRImage{
 		else{
 			endiannessStr = 1.0;
 		}
-		pfm.put(toUTF32!string("PF\n"~to!string(width)~" "~to!string(height)~"\n"~to!string(endiannessStr)~"\n"));
-
+		pfm.put(toUTF32("PF\n"~to!string(width)~" "~to!string(height)~"\n"~to!string(endiannessStr)~"\n")); 
 		color col;
 		for(int i=height-1; i>-1; i--){
 			for(int j=0; j<width; j++){
 				col = getPixel(j, i);
-				pfm.put(toUTF32!string(to!string(col.r)));
-				pfm.put(toUTF32!string(to!string(col.g)));
-				pfm.put(toUTF32!string(to!string(col.b)));
+				pfm.put(toUTF32(to!string(col.r)));
+				pfm.put(toUTF32(to!string(col.g)));
+				pfm.put(toUTF32(to!string(col.b)));}
 			}
-		}
-		writeln(pfm[]);
+		writeln(pfm); 
 	}
+
+	float averageLuminosity(float delta=1e-10){
+		float lumsum = 0.0;
+        foreach(p; pixels[]){
+            lumsum += log10(delta + p.luminosity());
+		}
+        return pow(10, lumsum / (pixels.length));
+		}
+
+
+	void normalizeImage(float factor, float luminosity){
+		if(luminosity!>0 && luminosity!<0 && luminosity!=0){
+			luminosity = averageLuminosity();
+			//luminosity ?? averageLuminosity(); // Tipi opzionali not found yet
+		}
+		for(int i=0; i<pixels.length; i++){
+			pixels[i] = pixels[i]*(factor/luminosity);
+		}
+	}
+
+	 void clampImage(){
+        for(int i=0; i< pixels.length; i++){
+            pixels[i].r = dClamp(pixels[i].r);
+            pixels[i].g = dClamp(pixels[i].g);
+            pixels[i].b = dClamp(pixels[i].b);
+		}
+	}
+
+}
 	/*unittest{
 		assert(toUTF8!string(11) == [0b00001011]);
 		assert(to!string(12.375).toUTF32.equal([0b01000001010001100000000000000000]));
 		}*/
-    
+    /*
 	unittest{
         HDRImage img = new HDRImage(7,4);
 
@@ -121,8 +155,63 @@ class HDRImage{
 		assert (! img.validCoordinates(0, 4));
 
 		assert (img.pixelOffset(3, 2) == 17);
-    }
-}
+    }*/
+
+	unittest
+	{ // Testing luminosity()
+    color col1 = {1.0, 2.0, 3.0};
+    color col2 = {9.0, 5.0, 7.0};
+
+    assert(areClose(2.0, col1.luminosity()));
+    assert(areClose(7.0, col2.luminosity()));
+	}
+
+	unittest
+	{ // Testing averageLuminosity()
+	HDRImage img1 = new HDRImage(2,1);
+	color c1 = {5.0,10.0,15.0};
+	color c2 = {500.0, 1000.0, 1500.0};
+	img1.setPixel(0,0, c1 );
+	img1.setPixel(1,0, c2);
+
+	writeln(img1.averageLuminosity(0.0));
+	assert(areClose(100.0, img1.averageLuminosity(0.0)));
+	}
+	
+	unittest
+	{ // Testing normalizeImage
+    HDRImage img2 = new HDRImage(2,1);
+
+	color c1 = {5.0,10.0,15.0};
+	color c2 = {500.0, 1000.0, 1500.0};
+	img2.setPixel(0,0, c1 );
+	img2.setPixel(1,0, c2);
+
+	color c3 = {0.5e2, 1.0e2, 1.5e2};
+	color c4 = {0.5e4, 1.0e4, 1.5e4};
+    img2.normalizeImage(1000.0,100.0);
+    assert(img2.getPixel(0, 0).colorAreClose(c3));
+    assert(img2.getPixel(1, 0).colorAreClose(c4));
+	}
+
+	unittest
+	{// Testing clumpImage
+	HDRImage img3 = new HDRImage(2,1);
+
+	color c1 = {0.5e1, 1.0e1, 1.5e1};
+	color c2 = {0.5e3, 1.0e3, 1.5e3};
+    img3.setPixel(0, 0, c1);
+    img3.setPixel(1, 0, c2);
+
+    img3.clampImage();
+
+    	// Check RGB boundaries
+		foreach(pixel;img3.pixels){
+			assert(pixel.r >= 0 && pixel.r <= 1);
+			assert(pixel.g >= 0 && pixel.g <= 1);
+			assert(pixel.b >= 0 && pixel.b <= 1);
+		}
+	}
 
 
 void main(string[] args)
@@ -139,7 +228,7 @@ void main(string[] args)
 
 	color c1 = {70,10,80};
 	color c2 = {20,100,33};
-	
+
 	color c3 = {3e-6, 0, 0};
 	writeln(c1.colorAreClose(c1+c3));
 
@@ -147,4 +236,8 @@ void main(string[] args)
 	image.setPixel(to!int(w/2),1,c2);
 	image.setPixel(0,to!int(h-1),c3);
 	image.writePFM();
+	//writeln(c1.luminosity());
+	float lum;
+	float fac=1.0;
+	image.normalizeImage(fac, lum);
 }
