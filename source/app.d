defccb0a81c5266c9e050baff593af7c44b92b1f
Luca Bernardi <sasasa@Air-di-Luca-2.homenet.telecomitalia.it>
2022-03-31 00:45:25 +0200

    We can't read the floats LoLoL

--- source/app.d
+++ source/app.d
@@ -2,7 +2,7 @@ import std.stdio;
 import std.conv;
 import std.math;
 import std.file;
-//import std.exception;
+import std.exception;
 import core.exception;
 import std.array;
 import std.string;
@@ -19,76 +19,107 @@ float clamp(float x){
 	return x/(1.0+x);
 }
 
-ubyte[] readLine(ubyte[] stream, uint startingPosition)
-in(startingPosition < stream.length, "Range error when reading a line."){
+ubyte[] readLine(ubyte[] stream, uint startingPosition){
 	ubyte[] line;
-
 	for(uint i=startingPosition; i<stream.length; i++){
 		line ~= stream[i];
 		if(stream[i] == 10) break;
 	}
-
 	return line;
 }
 
-int[2] parseImgSize(ubyte[] imgSize){
-	ubyte[][] dimensions = imgSize.split(10);
+unittest{
+	ubyte[] line = [72,101,108,108,111,10,119,111,114,108,100];
+	assert(readLine(line, 0) == [72,101,108,108,111,10]);
+	assert(readLine(line, 6) == [119,111,114,108,100]);
+	assert(line.readLine(11) == []);
+}
+
+int[2] parseImgSize(ubyte[] imgSize)
+in{
+	if(imgSize.length == 0){
+		throw new Exception(format("%s is an empty array.", imgSize));
+	}
+}
+do{
+	ubyte[][] dimensions = imgSize.split(32);
 	if(dimensions.length != 2){
-		throw new core.exception.RangeError("Invalid number of dimensions.");
+		throw new Exception("Invalid number of dimensions.");
+	}
+	if(dimensions[][0].length == 0 || dimensions[][1].length == 0){
+		throw new Exception("Invalid number of dimensions.");
 	}
 
-	string widthArray;
-	for(uint j=0; j<dimensions[][0].length; j++){
-		widthArray ~= to!string(dimensions[0][j]);
+	// Se ASCII esteso? Conversione a char[] fallisce con tipo std.utf.UTFException. Va controllato? Temo di sì.
+	char[] widthArray = cast(char[])(dimensions[][0]);
+	char[] heightArray = cast(char[])(dimensions[1][0..$-1]);
+	if(dimensions[1][$-1] != 10){
+		heightArray ~= cast(char)(dimensions[1][$-1]);
 	}
 
-	string heightArray;
-	for(uint j=0; j<dimensions[][1].length; j++){
-		heightArray ~= to!string(dimensions[1][j]);
-	}
-
-	int w = -1, h = -1;
 	try{
-		w = to!int(widthArray);
-		h = to!int(heightArray);
+		int w = to!int(widthArray);
+		int h = to!int(heightArray);
 		if(w < 0 || h < 0){
 			throw new object.Exception("Invalid width and/or height: they must be non negative.");
 		}
+		return [w,h];
 	}
 	catch(std.conv.ConvException exc){
-		writeln("Invalid width and/or height: they must be int.");
+		throw new std.conv.ConvException("Invalid width and/or height: they must be int.");
 	}
-	catch(core.exception.RangeError exc){
-		writeln(exc.msg);
-	}
-	catch(object.Exception exc){
-		writeln(exc.msg);
-	}
-
-	return [w, h];
 }
 
-float parseEndiannessLine(ubyte[] endiannessLine){
-	string endiannessArray;
-	for(uint j=0; j<endiannessLine.length; j++){
-		endiannessArray ~= to!string(endiannessLine[j]);
+unittest{
+	ubyte[] dimensionsLine = [51,32,50,10], dimensionsOnlyLine = [45,48,32,55,48];
+	assert(parseImgSize(dimensionsLine) == [3,2]);
+	assert(parseImgSize(dimensionsOnlyLine) == [0,70]);
+
+	ubyte[] floatDimensions = [50,46,32,51], negativeDimensions = [45,50,32,52];
+	ubyte[] manyDimensions = [53,32,53,32,49];
+	assertThrown!ConvException(parseImgSize(floatDimensions));
+	assertThrown!Exception(parseImgSize(negativeDimensions));
+	assertThrown!Exception(parseImgSize(manyDimensions));
+}
+
+float parseEndiannessLine(ubyte[] endiannessLine)
+in{
+	if(endiannessLine.length == 0){
+		throw new Exception(format("%s is an empty array.", endiannessLine));
+	}
+}
+do{
+	// Sempre problema se ASCII esteso.
+	char[] endiannessArray = cast(char[])(endiannessLine[0..$-1]);
+	if(endiannessLine[$-1] != 10){
+		endiannessArray ~= cast(char)(endiannessLine[$-1]);
 	}
 
-	float endianness;
 	try{
-		endianness = to!float(endiannessArray);
-		if(areClose(endianness,0,1e-20)){
+		float endiannessValue = to!float(endiannessArray);
+		if(areClose(endiannessValue,0,1e-20)){
 			throw new object.Exception("Endianness cannot be too close to zero.");
 		}
+		return endiannessValue;
 	}
 	catch(std.conv.ConvException exc){
-		writeln("Invalid endianness: it must be a float.");
+		throw new std.conv.ConvException("Invalid endianness: it must be a float.");
 	}
-	catch(object.Exception exc){
-		writeln(exc.msg);
-	}
+}
 
-	return endianness;
+unittest{
+	ubyte[] positiveNumber = [48,55,46,50,10], negativeNumber = [45,56,49];
+	assert(areClose(parseEndiannessLine(positiveNumber), 7.2));
+	assert(areClose(parseEndiannessLine(negativeNumber), -81));
+
+	ubyte[] zero = [48,46,48,48], epsilon = [46,48,48,48,48,48,48,48,48,48,48,48,50,10];
+	ubyte[] notNumber = [50,60,70,10];
+	assertThrown!Exception(parseEndiannessLine(zero));
+	assertNotThrown(parseEndiannessLine(epsilon));
+	assertThrown!ConvException(parseEndiannessLine(notNumber));
+
+	ubyte[] emptyArray = [];
+	assertThrown!Exception(parseEndiannessLine(emptyArray));
 }
 
 struct color{
@@ -120,23 +151,23 @@ struct color{
 	float luminosity(){
 		return (max(r, g, b) + min(r, g, b)) / 2.0;
 	}
+}
 
-	unittest{
-		color c1 = {1.0, 2.0, 3.0}, c2 = {5.0, 7.0, 9.0};
+unittest{
+	color c1 = {1.0, 2.0, 3.0}, c2 = {5.0, 7.0, 9.0};
 
-		assert (c1.colorIsClose(color(0.999999,2.0,3.0)));
+	assert(c1.colorIsClose(color(0.999999,2.0,3.0)));
 
-		assert ((c1+c2).colorIsClose(color(6.0, 9.0, 12.0)));
-		assert ((c1-c2).colorIsClose(color(-4.0, -5.0, -6.0)));
-		assert ((c1*c2).colorIsClose(color(5.0, 14.0, 27.0)));
+	assert((c1+c2).colorIsClose(color(6.0, 9.0, 12.0)));
+	assert((c1-c2).colorIsClose(color(-4.0, -5.0, -6.0)));
+	assert((c1*c2).colorIsClose(color(5.0, 14.0, 27.0)));
 
-		assert ((c1*2.0).colorIsClose(color(2.0, 4.0, 6.0)));
-		assert ((3.0*c1).colorIsClose(color(3.0, 6.0, 9.0)));
+	assert((c1*2.0).colorIsClose(color(2.0, 4.0, 6.0)));
+	assert((3.0*c1).colorIsClose(color(3.0, 6.0, 9.0)));
 		
-		color c3 = {9.0, 5.0, 7.0};
-		assert(areClose(2.0, c1.luminosity()));
-		assert(areClose(7.0, c3.luminosity()));
-	}
+	color c3 = {9.0, 5.0, 7.0};
+	assert(areClose(2.0, c1.luminosity()));
+	assert(areClose(7.0, c3.luminosity()));
 }
 
 class HDRImage{
@@ -149,30 +180,34 @@ class HDRImage{
 		pixels.length = width*height;
 	}
 
-	this(ubyte[] stream){
-		/*++i; // incremento perché i è sullo \n
+	this(int w, int h, ubyte[] stream, float endiannessValue){
+		if(12*w*h != stream.length){
+			throw new Exception(format("Expected %s pixels", w*h));
+		}
+		this(w, h);
 
-		if(endiannessLittle){
-			for(uint j=0; j<width*height; j++){
-				reverse(stream[i..i+12]);
-				pixels[j].b = *cast(float*)(&stream[i]);
-				pixels[j].g = *cast(float*)(&stream[i+4]);
-				pixels[j].r = *cast(float*)(&stream[i+8]);
-				i += 12;
+		if(areClose(endiannessValue,0,1e-20)){
+			throw new object.Exception("Endianness cannot be too close to zero.");
+		}
+		else if(endiannessValue < 0){
+			for(auto j=pixels.length-1; j>-1; j--){
+				reverse(stream[j..j+12]);
+				pixels[j].b = *cast(float*)(&stream[j-12]);
+				pixels[j].g = *cast(float*)(&stream[j-8]);
+				pixels[j].r = *cast(float*)(&stream[j-4]);
 			}
 		}
 		else{
-			for(uint j=0; j<width*height; j++){
-				pixels[j].r = *cast(float*)(&stream[i]);
-				pixels[j].g = *cast(float*)(&stream[i+4]);
-				pixels[j].b = *cast(float*)(&stream[i+8]);
-				i += 12;
+			for(auto j=pixels.length-1; j>-1; j--){
+				pixels[j].r = *cast(float*)(&stream[j-12]);
+				pixels[j].g = *cast(float*)(&stream[j-8]);
+				pixels[j].b = *cast(float*)(&stream[j-4]);
 			}
-		}*/
+		}
 	}
 
 	this(string fileName){
-		ubyte[] stream = cast(ubyte[])fileName.read;
+		ubyte[] stream = cast(ubyte[])(fileName.read);
 		int streamPosition = 0;
 
 		ubyte[] magic = stream.readLine(streamPosition);
@@ -183,15 +218,13 @@ class HDRImage{
 
 		ubyte[] imgSize = stream.readLine(streamPosition);
 		int[2] size = parseImgSize(imgSize);
-		this(size[0], size[1]);
 		streamPosition += imgSize.length;
 
 		ubyte[] endiannessLine = stream.readLine(streamPosition);
-		float endianness = parseEndiannessLine(endiannessLine);
+		float endiannessValue = parseEndiannessLine(endiannessLine);
+		streamPosition += endiannessLine.length;
 
-		if(12*width*height != stream.length-streamPosition){
-			throw new Exception(format("Expected %s pixels", width*height));
-		}
+		this(size[0], size[1], stream[streamPosition..$], endiannessValue);
 	}
 
 	bool validCoordinates(int x, int y){
@@ -267,49 +300,62 @@ class HDRImage{
 			pixels[i].b = clamp(pixels[i].b);
 		}
 	}
+}
 
-	unittest{
+unittest{
         HDRImage img = new HDRImage(7,4);
 
-		assert (img.validCoordinates(0, 0)); 
-		assert (img.validCoordinates(6, 3));
-		assert (!img.validCoordinates(-1, 0));
-		assert (!img.validCoordinates(0, -1));
-		assert (!img.validCoordinates(7, 0));
-		assert (!img.validCoordinates(0, 4));
+		assert(img.validCoordinates(0, 0)); 
+		assert(img.validCoordinates(6, 3));
+		assert(!img.validCoordinates(-1, 0));
+		assert(!img.validCoordinates(0, -1));
+		assert(!img.validCoordinates(7, 0));
+		assert(!img.validCoordinates(0, 4));
 
-		assert (img.pixelOffset(3, 2) == 17);
+		assert(img.pixelOffset(3, 2) == 17);
     }
 
-	unittest{
-		HDRImage img = new HDRImage(2,1);
-		color c1 = {5.0, 10.0, 15.0}, c2 = {500.0, 1000.0, 1500.0};
-		img.setPixel(0, 0, c1);
-		img.setPixel(1, 0, c2);
+unittest{
+	HDRImage img = new HDRImage(2,1);
+	color c1 = {5.0, 10.0, 15.0}, c2 = {500.0, 1000.0, 1500.0};
+	img.setPixel(0, 0, c1);
+	img.setPixel(1, 0, c2);
 		
-		writeln(img.averageLuminosity(0.0));
-		assert(areClose(100.0, img.averageLuminosity(0.0)));
+	writeln(img.averageLuminosity(0.0));
+	assert(areClose(100.0, img.averageLuminosity(0.0)));
 		
-		color c3 = {0.5e2, 1.0e2, 1.5e2}, c4 = {0.5e4, 1.0e4, 1.5e4};
-		img.normalizeImage(1000.0, 100.0);
-		assert(img.getPixel(0, 0).colorIsClose(c3));
-		assert(img.getPixel(1, 0).colorIsClose(c4));
-	}
+	color c3 = {0.5e2, 1.0e2, 1.5e2}, c4 = {0.5e4, 1.0e4, 1.5e4};
+	img.normalizeImage(1000.0, 100.0);
+	assert(img.getPixel(0, 0).colorIsClose(c3));
+	assert(img.getPixel(1, 0).colorIsClose(c4));
+}
 
-	unittest{
-		HDRImage img = new HDRImage(2,1);
+unittest{
+	HDRImage img = new HDRImage(2, 1);
 		
-		color c1 = {0.5e1, 1.0e1, 1.5e1}, c2 = {0.5e3, 1.0e3, 1.5e3};
-		img.setPixel(0, 0, c1);
-		img.setPixel(1, 0, c2);
-		img.clampImage();
+	color c1 = {0.5e1, 1.0e1, 1.5e1}, c2 = {0.5e3, 1.0e3, 1.5e3};
+	img.setPixel(0, 0, c1);
+	img.setPixel(1, 0, c2);
+	img.clampImage();
 		
-		// Check RGB boundaries
-		foreach(pixel; img.pixels){
-			assert(pixel.r >= 0 && pixel.r <= 1);
-			assert(pixel.g >= 0 && pixel.g <= 1);
-			assert(pixel.b >= 0 && pixel.b <= 1);
-		}
+	// Check RGB boundaries
+	foreach(pixel; img.pixels){
+		assert(pixel.r >= 0 && pixel.r <= 1);
+		assert(pixel.g >= 0 && pixel.g <= 1);
+		assert(pixel.b >= 0 && pixel.b <= 1);
+	}
+}
+
+unittest{
+	string[2] files = ["reference_le.pfm", "reference_be.pfm"];
+
+	foreach(string fileName; files){
+		HDRImage img = new HDRImage(files[1]);
+
+		assert(img.width == 3);
+		assert(img.height == 2);
+
+		//assert(img.getPixel(0,0).colorIsClose(color(1.0e1, 2.0e1, 3.0e1)));
 	}
 }
 
