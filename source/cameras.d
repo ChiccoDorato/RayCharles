8b5486a58057805c58f6715ff3798b60c09811a2
Luca Bernardi <luhabernardi@gmail.com>
2022-04-17 23:30:38 +0200

    Standardized writing

--- source/cameras.d
+++ source/cameras.d
@@ -1,37 +1,34 @@
 import geometry;
-import std.math;
 
 struct Ray
 {
     Point origin;
     Vec dir;
-    float tMin = 1e-5;
-    float tMax = float.infinity; 
+    float tMin = 1e-5, tMax = float.infinity; 
     int depth = 0;
 
     Point at(float t)
     {
-        return origin + t*dir;
+        return origin + t * dir;
     }
 
-    bool isClose(Ray rhs, float epsilon = 1e-5)
+    bool rayIsClose(Ray rhs)
     {
-        return(origin.xyzIsClose(rhs.origin) && dir.xyzIsClose(rhs.dir));
+        return origin.xyzIsClose(rhs.origin) && dir.xyzIsClose(rhs.dir);
     }
 }
 
 unittest
 {
-    Ray ray1 = Ray(Point(1.0, 2.0, 3.0), Vec(5.0, 4.0, -1.0));
-    Ray ray2 = Ray(Point(1.0, 2.0, 3.0), Vec(5.0, 4.0, -1.0));
-    Ray ray3 = Ray(Point(5.0, 1.0, 4.0), Vec(3.0, 9.0, 4.0));
+    Ray r1 = {Point(1.0, 2.0, 3.0), Vec(5.0, 4.0, -1.0)};
+    Ray r2 = {Point(1.0, 2.0, 3.0), Vec(5.0, 4.0, -1.0)};
+    Ray r3 = {Point(5.0, 1.0, 4.0), Vec(3.0, 9.0, 4.0)};
 
-    assert(ray1.isClose(ray2));
-    assert(!ray1.isClose(ray3));
+    assert(r1.rayIsClose(r2));
+    assert(!r1.rayIsClose(r3));
 
-    
-    Ray ray4 = Ray(Point(1.0, 2.0, 4.0), Vec(4.0, 2.0, 1.0));
-    assert(ray4.at(0.0).xyzIsClose(ray4.origin));
-    assert(ray4.at(1.0).xyzIsClose(Point(5.0, 4.0, 5.0)));
-    assert(ray4.at(2.0).xyzIsClose(Point(9.0, 6.0, 6.0)));
+    Ray r4 = {Point(1.0, 2.0, 4.0), Vec(4.0, 2.0, 1.0)};
+    assert(r4.at(0.0).xyzIsClose(r4.origin));
+    assert(r4.at(1.0).xyzIsClose(Point(5.0, 4.0, 5.0)));
+    assert(r4.at(2.0).xyzIsClose(Point(9.0, 6.0, 6.0)));
 }
