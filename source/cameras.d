8459818aad692efac3aa4782b7ed8b847f33a12e
Luca Bernardi <65958243+ChiccoDorato@users.noreply.github.com>
2022-04-26 12:41:48 +0200

    Cameras (#2)
    * Few Changes read/write
    
    * notYetGeometry
    
    * Completed Transformation and std style
    
    * Removing local import
    
    * First Commit Cameras
    
    * General update
    
    * Updating .gitignore
    
    * Adding automatic tests
    
    * Pfm files for tests
    
    * Standardized writing
    
    * Removing executable
    
    * Adding cameras.d: Rays, Camera, OrthogonalCamera and PerspectiveCamera + All Tests working. Missing ImageTracer
    
    * Adding ImageTracer - fireAllRays incomplete
    
    * Adding ImageTracer - fireAllRays to end
    
    * cameras: fireRay unittest corrected - fireAllRays still to adjust
    
    * Completing ImageTracer
    
    * Error handling
    
    Co-authored-by: Frastandreetto <francesco.andreetto97@gmail.com>

---
+++ source/cameras.d
@@ -0,0 +1,198 @@
+import geometry;
+import hdrimage;
+import transformations;
+
+struct Ray
+{
+    Point origin;
+    Vec dir;
+    float tMin = 1e-5, tMax = float.infinity; 
+    int depth = 0;
+
+    Point at(float t)
+    {
+        return origin + t * dir;
+    }
+
+    bool rayIsClose(Ray rhs)
+    {
+        return origin.xyzIsClose(rhs.origin) && dir.xyzIsClose(rhs.dir);
+    }
+}
+
+unittest
+{
+    Ray r1 = {Point(1.0, 2.0, 3.0), Vec(5.0, 4.0, -1.0)};
+    Ray r2 = {Point(1.0, 2.0, 3.0), Vec(5.0, 4.0, -1.0)};
+    Ray r3 = {Point(5.0, 1.0, 4.0), Vec(3.0, 9.0, 4.0)};
+
+    assert(r1.rayIsClose(r2));
+    assert(!r1.rayIsClose(r3));
+
+    Ray r4 = {Point(1.0, 2.0, 4.0), Vec(4.0, 2.0, 1.0)};
+    assert(r4.at(0.0).xyzIsClose(r4.origin));
+    assert(r4.at(1.0).xyzIsClose(Point(5.0, 4.0, 5.0)));
+    assert(r4.at(2.0).xyzIsClose(Point(9.0, 6.0, 6.0)));
+}
+
+unittest
+{
+    Ray r = {Point(1.0, 2.0, 3.0), Vec(6.0, 5.0, 4.0)};
+    Transformation t = translation(Vec(10.0, 11.0, 12.0)) * rotationX(90.0);
+    Ray transformed = t * r;
+
+    assert(transformed.origin.xyzIsClose(Point(11.0, 8.0, 14.0)));
+    assert(transformed.dir.xyzIsClose(Vec(6.0, -4.0, 5.0)));
+}
+
+class Camera
+{   
+    float d; // Screen-observer distance
+    float aspectRatio;
+    Transformation transformation;  
+     
+    abstract Ray fireRay(float u, float v);
+}
+
+class OrthogonalCamera : Camera 
+{
+    this(float aspRat = 1.0, Transformation transf = Transformation())
+    in (aspRat > 0)
+    {
+        aspectRatio = aspRat;
+        transformation = transf;
+    }
+
+    override Ray fireRay(float u, float v)
+    {
+        Ray r = {Point(-1.0, (1.0 - 2 * u) * aspectRatio, 2 * v - 1), vecX};
+        r = transformation * r;
+        r.tMin = 1.0;
+        return r;
+    }
+}
+
+unittest
+{
+    OrthogonalCamera cam = new OrthogonalCamera(2.0);
+
+    Ray r1 = cam.fireRay(0.0, 0.0);
+    Ray r2 = cam.fireRay(1.0, 0.0);
+    Ray r3 = cam.fireRay(0.0, 1.0);
+    Ray r4 = cam.fireRay(1.0, 1.0);
+
+    // Rays are parallel if their cross product vanishes
+    assert(areClose(0.0, (r1.dir ^ r2.dir).squaredNorm));
+    assert(areClose(0.0, (r1.dir ^ r3.dir).squaredNorm));
+    assert(areClose(0.0, (r1.dir ^ r4.dir).squaredNorm));
+
+   // Rays hitting the corners have the right coordinates
+    assert(r1.at(1.0).xyzIsClose(Point(0.0, 2.0, -1.0)));
+    assert(r2.at(1.0).xyzIsClose(Point(0.0, -2.0, -1.0)));
+    assert(r3.at(1.0).xyzIsClose(Point(0.0, 2.0, 1.0)));
+    assert(r4.at(1.0).xyzIsClose(Point(0.0, -2.0, 1.0)));
+}
+
+unittest
+{
+    Camera cam = new OrthogonalCamera(1.0, translation(-vecY * 2.0) * rotationZ(90));
+    Ray r = cam.fireRay(0.5, 0.5);
+    assert(r.at(1.0).xyzIsClose(Point(0.0, -2.0, 0.0)));
+}
+
+ class PerspectiveCamera : Camera 
+{
+    this(float dist = 1.0, float aspRat = 1.0, Transformation transf = Transformation())
+    in (dist > 0 && aspRat > 0)
+    {
+        d = dist;
+        aspectRatio = aspRat;
+        transformation = transf;
+    }
+
+    override Ray fireRay(float u, float v)
+    {
+        Ray r = {Point(-d, 0.0, 0.0), Vec(d, (1.0 - 2 * u) * aspectRatio, 2 * v - 1)};
+        r = transformation * r;
+        r.tMin = 1.0;
+        return r;
+    }
+}
+
+unittest
+{
+    Camera cam = new PerspectiveCamera(1.0, 2.0);
+
+    Ray r1 = cam.fireRay(0.0, 0.0);
+    Ray r2 = cam.fireRay(1.0, 0.0);
+    Ray r3 = cam.fireRay(0.0, 1.0);
+    Ray r4 = cam.fireRay(1.0, 1.0);
+
+    // All the rays depart from the same point
+    assert(r1.origin.xyzIsClose(r2.origin));
+    assert(r1.origin.xyzIsClose(r3.origin));
+    assert(r1.origin.xyzIsClose(r4.origin));
+
+    // The ray hitting the corners have the right coordinates
+    assert(r1.at(1.0).xyzIsClose(Point(0.0, 2.0, -1.0)));
+    assert(r2.at(1.0).xyzIsClose(Point(0.0, -2.0, -1.0)));
+    assert(r3.at(1.0).xyzIsClose(Point(0.0, 2.0, 1.0)));
+    assert(r4.at(1.0).xyzIsClose(Point(0.0, -2.0, 1.0)));
+}
+
+unittest
+{
+    Camera cam = new PerspectiveCamera(1.0, 1.0, translation(-vecY * 2.0) * rotationZ(90));
+    Ray r = cam.fireRay(0.5, 0.5);
+    assert(r.at(1.0).xyzIsClose(Point(0.0, -2.0, 0.0)));
+}
+
+class ImageTracer
+{
+    HDRImage image;
+    Camera camera;
+
+    this(HDRImage img, Camera cam)
+    {
+        image = img;
+        camera = cam;
+    }
+
+    Ray fireRay(int col, int row, float uPixel = 0.5, float vPixel = 0.5)
+    {
+        // For now, there is an error in this formula
+        float u = (col + uPixel) / (image.width - 1);
+        float v = (row + vPixel) / (image.height - 1);
+        return camera.fireRay(u, v);
+    }
+
+    void fireAllRays(Color function(Ray) solveRendering)
+    {
+        Ray ray;
+        Color color;
+
+        for (uint row = 0; row < image.height; ++row){
+            for (uint col = 0; col < image.width; ++col){
+                ray = fireRay(col, row);
+                color = solveRendering(ray);
+                image.setPixel(col, row, color);
+            }
+        }
+    }
+}
+
+unittest
+{
+    HDRImage image = new HDRImage(4, 2);
+    Camera camera = new PerspectiveCamera(1.0, 2.0);
+    ImageTracer tracer = new ImageTracer(image, camera);
+
+    Ray r1 = tracer.fireRay(0, 0, 2.5, 1.5);
+    Ray r2 = tracer.fireRay(2, 1, 0.5, 0.5);
+    assert(r1.rayIsClose(r2));
+    
+    tracer.fireAllRays(r => Color(1.0, 2.0, 3.0));
+    for (uint row = 0; row < image.height; ++row)
+        for (uint col = 0; col < image.width; ++col)
+            assert(image.getPixel(col, row) == Color(1.0, 2.0, 3.0));
+}
