1c18234e954ad4f58832ad520055a895ae92c130
Frastandreetto <francesco.andreetto97@gmail.com>
2022-05-25 11:52:47 +0200

    Adding InputStream

--- source/tokens.d
+++ source/tokens.d
@@ -1,6 +1,26 @@
 module tokens;
 
+import std.array : Appender;
+
 union SourceLocation
 {
     //
+}
+
+struct InputStream 
+{
+    char[] stream;
+    char savedChar;
+    SourceLocation location, savedLocation;
+    ubyte tabulations;
+
+    this(in char[] s, in string fileName = "", in ubyte tab = 8) pure nothrow
+    in (tab == 4 || tab == 8)
+    {
+        stream = s;
+        location = SourceLocation(fileName, 1, 1);
+        savedChar = '';
+        savedLocation = location;
+        tabulations = tab;
+    }
 }
