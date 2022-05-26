da9b7ef0d3885eedfeec1720464066576674cfdc
Frastandreetto <francesco.andreetto97@gmail.com>
2022-05-26 14:24:29 +0200

    Adding cylinder

--- source/tokens.d
+++ source/tokens.d
@@ -1,4 +1,4 @@
-module tokens;
+/* module tokens;
 
 struct SourceLocation
 {
@@ -10,17 +10,57 @@ struct SourceLocation
 struct InputStream 
 {
     char[] stream;
+    uint index = 0;
     char savedChar;
     SourceLocation location, savedLocation;
     ubyte tabulations;
 
-    this(in char[] s, in string fileName = "", in ubyte tab = 8) pure nothrow
+    this(in char[] s, in string fileName = "", in ubyte tab = 4) pure nothrow
+    in (stream.length !=0)
     in (tab == 4 || tab == 8)
     {
         stream = s;
         location = SourceLocation(fileName, 1, 1);
-        savedChar = '';
+        savedChar = char.init;
         savedLocation = location;
         tabulations = tab;
     }
-}
+    
+    void updatePos(in char c) pure nothrow
+    {
+        if (c == char.init) return;
+        if (c == '\n') 
+        {
+            ++location.line;
+            location.col = 1;
+        }
+        else if (c == '\t') location.col += tabulations;
+        else ++location.col;
+    }
+
+    char read() pure nothrow
+    {
+        char c;
+        if(savedChar == char.init)
+        {
+            c = stream[index];
+            ++index;
+        }
+        else   
+        {
+            c = savedChar;
+            savedChar = char.init;
+        }
+    
+        savedLocation = location;
+        updatePos(c);
+    }
+
+    void unreadChar(in char c) pure nothrow
+    in(c == char.init)
+    {
+        savedChar = c;
+        location = savedLocation;
+        --index;
+    }
+} */
