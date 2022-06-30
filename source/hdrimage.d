a43fb6f6cc694e93e8b7a0097d758b2da9c634b5
Luca Bernardi <luhabernardi@gmail.com>
2022-06-30 19:50:52 +0200

    In hdrimage.d: toString, nogc review and better formatting

--- source/hdrimage.d
+++ source/hdrimage.d
@@ -2,13 +2,14 @@ module hdrimage;
 
 import imageformats.png;
 import std.algorithm : endsWith, max, min;
-import std.array : Appender, appender, split;
+import std.array : appender, split;
 import std.bitmanip;
 import std.conv;
 import std.exception : assertThrown, assertNotThrown, enforce;
 import std.file : read;
-import std.format : format;
-import std.math : abs, isNaN, log10, NaN, pow, round;
+import std.format;
+import std.math : abs, isNaN, log10, pow, round;
+import std.range : isOutputRange, put;
 import std.stdio : File, writeln;
 import std.system : endian;
 
@@ -25,29 +26,38 @@ struct Color
 	float r = 0.0, g = 0.0, b = 0.0;
 
 	/// Return the sum (+), the difference (-) or the product (*) between two Colors
-	pure nothrow @nogc @safe Color opBinary(string op)(in Color rhs) const if (op == "+" || op == "-" || op == "*")
+	pure nothrow @nogc @safe Color opBinary(string op)(in Color rhs) const
+	if (op == "+" || op == "-" || op == "*")
 	{
-		mixin("return Color(r" ~ op ~ "rhs.r, g" ~ op ~ "rhs.g, b" ~ op ~ "rhs.b);");
+		return mixin("Color(r" ~ op ~ "rhs.r, g" ~ op ~ "rhs.g, b" ~ op ~ "rhs.b)");
 	}
 
 	/// Return the product between a floating point alpha on the left-hand side and a Color
-	pure nothrow @nogc @safe Color opBinary(string op)(in float alfa) const if (op == "*")
+	pure nothrow @nogc @safe Color opBinary(string op)(in float alfa) const
+	if (op == "*")
 	{
-		mixin("return Color(r*alfa, g*alfa, b*alfa);");
+		return mixin("Color(r*alfa, g*alfa, b*alfa)");
 	}
 
 	/// Return the product between a floating point alpha on the right-hand side and a Color
-	pure nothrow @nogc @safe Color opBinaryRight(string op)(in float alfa) const if (op == "*")
+	pure nothrow @nogc @safe Color opBinaryRight(string op)(in float alfa) const
+	if (op == "*")
 	{
-		mixin("return Color(alfa*r, alfa*g, alfa*b);");
+		return mixin("Color(alfa*r, alfa*g, alfa*b)");
 	}
 
 	/// Return the three components of a Color in a string.
-	// gdc throws error: not pure. Ok for dmd and ldc2.
-	pure @safe string toString() const
-	{
-		return "<r: " ~ to!string(r) ~ ", g: " ~ to!string(g) ~ ", b: " ~ to!string(b) ~ ">";
-	}
+	@safe void toString(W)(ref W w, scope const ref FormatSpec!char fmt) const
+    if (isOutputRange!(W, char))
+    {
+		put(w, "<");
+        formatValue(w, r, fmt);
+        put(w, ", ");
+        formatValue(w, g, fmt);
+		put(w, ", ");
+        formatValue(w, b, fmt);
+        put(w, ">");
+    }
 
 	/// Verify if two Colors are close by calling the fuction areClose for every component 
 	pure nothrow @nogc @safe bool colorIsClose(in Color c) const
@@ -63,34 +73,12 @@ struct Color
 }
 
 /// Fundamental colors: black (0.0, 0.0, 0.0) and white (1.0, 1.0, 1.0)
-immutable Color black = Color(), white = Color(1.0, 1.0, 1.0);
+immutable black = Color(), white = Color(1.0, 1.0, 1.0);
 
 ///
 unittest
 {
-	string[2] files = ["reference_le.pfm", "reference_be.pfm"];
-
-	foreach (string fileName; files)
-	{
-		HDRImage img = new HDRImage(fileName);
-		// HDRImage
-		assert(img.width == 3);
-		assert(img.height == 2);
-		// colorIsClose
-		assert(img.getPixel(0,0).colorIsClose(Color(1.0e1, 2.0e1, 3.0e1)));
-		assert(img.getPixel(1,0).colorIsClose(Color(4.0e1, 5.0e1, 6.0e1)));
-		assert(img.getPixel(2,0).colorIsClose(Color(7.0e1, 8.0e1, 9.0e1)));
-		assert(img.getPixel(0,1).colorIsClose(Color(1.0e2, 2.0e2, 3.0e2)));
-		assert(img.getPixel(0,0).colorIsClose(Color(1.0e1, 2.0e1, 3.0e1)));
-		assert(img.getPixel(1,1).colorIsClose(Color(4.0e2, 5.0e2, 6.0e2)));
-		assert(img.getPixel(2,1).colorIsClose(Color(7.0e2, 8.0e2, 9.0e2)));
-	}
-}
-
-///
-unittest
-{
-	Color c1 = {1.0, 2.0, 3.0}, c2 = {5.0, 7.0, 9.0};
+	auto c1 = Color(1.0, 2.0, 3.0), c2 = Color(5.0, 7.0, 9.0);
 
 	// colorIsClose
 	assert(c1.colorIsClose(Color(0.999999, 2.0, 3.0)));
@@ -104,7 +92,7 @@ unittest
 	assert((c1 * 2.0).colorIsClose(Color(2.0, 4.0, 6.0)));
 	assert((3.0 * c1).colorIsClose(Color(3.0, 6.0, 9.0)));
 		
-	Color c3 = {9.0, 5.0, 7.0};
+	auto c3 = Color(9.0, 5.0, 7.0);
 	// luminosity
 	assert(areClose(2.0, c1.luminosity));
 	assert(areClose(7.0, c3.luminosity));
@@ -113,7 +101,7 @@ unittest
 /// Class used to throw exceptions in case of compilation errors
 class InvalidPFMFileFormat : Exception
 {
-    pure @nogc @safe this(string msg, string file = __FILE__, size_t line = __LINE__)
+    pure nothrow @nogc @safe this(string msg, string file = __FILE__, size_t line = __LINE__)
     {
         super(msg, file, line);
     }
@@ -122,13 +110,13 @@ class InvalidPFMFileFormat : Exception
 /// Read a line from an array of ubyte, given a starting position
 pure nothrow @safe ubyte[] readLine(in ubyte[] stream, in uint startingPosition)
 {
-	ubyte[] line;
+	auto line = appender!(ubyte[]);
 	for (uint i = startingPosition; i < stream.length; ++i)
 	{
-		line ~= stream[i];
+		line.put(stream[i]);
 		if (stream[i] == 10) break;
 	}
-	return line;
+	return line.data;
 }
 
 ///
@@ -145,28 +133,39 @@ unittest
 /// Check the width and the height of the image, throw an exception if something is wrong.
 pure @safe int[2] parseImgSize(in ubyte[] imgSize)
 {
-	enforce!InvalidPFMFileFormat(imgSize.length > 0, "image dimensions are not indicated");
+	enforce!InvalidPFMFileFormat(
+		imgSize.length > 0,
+		"image dimensions are not indicated"
+		);
 
 	const(ubyte[][]) dimensions = imgSize.split(32);
-	if (dimensions.length != 2)
-		throw new InvalidPFMFileFormat("invalid number of dimensions");
-	if (dimensions[][0].length == 0 || dimensions[][1].length == 0)
-		throw new InvalidPFMFileFormat("invalid number of dimensions");
+	enforce!InvalidPFMFileFormat(
+		dimensions.length == 2,
+		"invalid number of dimensions"
+		);
+	enforce!InvalidPFMFileFormat(
+		dimensions[][0].length != 0 && dimensions[][1].length != 0,
+		"invalid number of dimensions"
+		);
 
 	// Se ASCII esteso - Conversione a char[] fallisce con tipo std.utf.UTFException.
-	const char[] widthArray = cast(const(char)[])dimensions[][0];
-	const char[] heightArray = cast(const(char)[])dimensions[1][0 .. $-1];
+	auto widthArray = cast(const(char)[])dimensions[][0];
+	auto heightArray = cast(const(char)[])dimensions[1][0 .. $-1];
 
 	try
 	{
-		immutable int w = to!int(widthArray);
-		immutable int h = to!int(heightArray);
-		if (w <= 0 || h <= 0)
-			throw new InvalidPFMFileFormat("invalid width and/or height (non positive)");
+		immutable w = to!int(widthArray);
+		immutable h = to!int(heightArray);
+		enforce!InvalidPFMFileFormat(
+			w > 0 && h > 0,
+			"invalid width and/or height (non positive)"
+			);
 		return [w, h];
 	}
 	catch (ConvException exc)
-		throw new InvalidPFMFileFormat("invalid width and/or height (not an integer)");
+		throw new InvalidPFMFileFormat(
+			"invalid width and/or height (not an integer)"
+			);
 }
 
 ///
@@ -191,19 +190,26 @@ unittest
 /// Check if the correct Endianness is used, throw an exception if wrong.
 pure @safe float parseEndiannessLine(in ubyte[] endiannessLine)
 {
-	enforce!InvalidPFMFileFormat(endiannessLine.length > 0, "endianness is not indicated");
+	enforce!InvalidPFMFileFormat(
+		endiannessLine.length > 0,
+		"endianness is not indicated"
+		);
 	// Sempre problema se ASCII esteso.
-	const char[] endiannessArray = cast(const(char)[])(endiannessLine[0 .. $-1]);
+	auto endiannessArray = cast(const(char)[])(endiannessLine[0 .. $-1]);
 
 	try
 	{
-		immutable float endiannessValue = to!float(endiannessArray);
+		immutable endiannessValue = to!float(endiannessArray);
 		if (areClose(endiannessValue, 0, 1e-20))
-			throw new InvalidPFMFileFormat("endianness cannot be too close to zero");
+			throw new InvalidPFMFileFormat(
+				"endianness cannot be too close to zero"
+				);
 		return endiannessValue;
 	}
 	catch (ConvException exc)
-		throw new InvalidPFMFileFormat("invalid endianness (not a floating point)");
+		throw new InvalidPFMFileFormat(
+			"invalid endianness (not a floating point)"
+			);
 }
 ///
 unittest
@@ -225,8 +231,8 @@ unittest
 /// Read a floating point number from an array of ubyte.
 pure float readFloat(in ubyte[] stream, in int startingPosition, in float endiannessValue)
 in (stream.length - startingPosition > 3,
-	format("Less than 4 bytes in %s from index %s", stream, startingPosition))
-in (!areClose(endiannessValue, 0, 1e-20), "Endianness cannot be too close to zero")
+	format("Only %s bytes left in the stream", stream.length - startingPosition))
+in (!areClose(endiannessValue, 0, 1e-20), "Endianness must differ from zero")
 {
 	uint nativeValue = *cast(uint*)(stream.ptr + startingPosition);
 	if (endian == Endian.littleEndian && endiannessValue > 0)
@@ -271,36 +277,36 @@ class HDRImage
 	/// Build an HDRImage from an array of ubyte
 	pure this(in ubyte[] stream)
 	{
-		int streamPosition = 0;
+		int streamPos = 0;
 
-		immutable ubyte[] magic = stream.readLine(streamPosition);
+		immutable ubyte[] magic = stream.readLine(streamPos);
 		enforce!InvalidPFMFileFormat(magic == [80, 70, 10], "invalid magic");
-		streamPosition += magic.length;
+		streamPos += magic.length;
 
-		immutable ubyte[] imgSize = stream.readLine(streamPosition);
+		immutable ubyte[] imgSize = stream.readLine(streamPos);
 		immutable int[2] size = parseImgSize(imgSize);
-		streamPosition += imgSize.length;
+		streamPos += imgSize.length;
 
-		immutable ubyte[] endiannessLine = stream.readLine(streamPosition);
+		immutable ubyte[] endiannessLine = stream.readLine(streamPos);
 		immutable float endiannessValue = parseEndiannessLine(endiannessLine);
-		streamPosition += endiannessLine.length;
+		streamPos += endiannessLine.length;
 
 		enforce!InvalidPFMFileFormat(
-			12 * size[0] * size[1] == stream.length - streamPosition,
-			format("expected [%s] pixels", size[0]*size[1])
+			12 * size[0] * size[1] == stream.length - streamPos,
+			format("expected %s pixels", size[0]*size[1])
 			);
 		this(size[0], size[1]);
 
 		float red, green, blue;
-		int posPixel;
+		int pixelPos;
 		for (int i = 0; i < height; ++i)
 		{
 			for (int j = 0; j < width; ++j)
 			{
-				posPixel = streamPosition + 12 * this.pixelOffset(j, height - 1 - i);
-				red = readFloat(stream, posPixel, endiannessValue);
-				green = readFloat(stream, posPixel + 4, endiannessValue);
-				blue = readFloat(stream, posPixel + 8, endiannessValue);
+				pixelPos = streamPos + 12 * this.pixelOffset(j, height - 1 - i); //(j, i)
+				red = readFloat(stream, pixelPos, endiannessValue);
+				green = readFloat(stream, pixelPos + 4, endiannessValue);
+				blue = readFloat(stream, pixelPos + 8, endiannessValue);
 				this.setPixel(j, i, Color(red, green, blue));
 			}
 		}
@@ -309,7 +315,7 @@ class HDRImage
 	/// Build an HDRImage from a file
 	this(in string fileName)
 	{
-		immutable(ubyte[]) stream = cast(immutable(ubyte[]))(fileName.read);
+		auto stream = cast(immutable ubyte[])(fileName.read);
 		this(stream);
 	}
 
@@ -326,14 +332,14 @@ class HDRImage
 	}
 	
 	/// Return the Color of a Pixel if it is inside the HDRImage
-	pure nothrow @nogc @safe Color getPixel(in int x, in int y) const
+	pure nothrow @safe Color getPixel(in int x, in int y) const
 	in (validCoordinates(x, y))
 	{
 		return pixels[pixelOffset(x, y)];
 	}
 
 	/// Set the Color given in a specific Pixel defined by two integer coordinates (x and y) 
-	pure nothrow @nogc @safe void setPixel(in int x, in int y, Color c)
+	pure nothrow @safe void setPixel(in int x, in int y, Color c)
 	in (validCoordinates(x, y))
 	{
 		pixels[pixelOffset(x, y)] = c;
@@ -344,30 +350,39 @@ class HDRImage
 	{
 		immutable ubyte[3] magic = [80, 70, 10], endOfHeader = [49, 46, 48];
 		immutable ubyte space = 32, endLine = 10;
-		ubyte[] byteWidth = cast(ubyte[])(to!(char[])(width)), byteHeight = cast(ubyte[])(to!(char[])(height));
+		auto byteWidth = cast(ubyte[])(to!(char[])(width));
+		auto byteHeight = cast(ubyte[])(to!(char[])(height));
 
-		Appender!(ubyte[]) pfm = appender!(ubyte[]);
+		auto pfm = appender!(ubyte[]);
 		if (endianness == Endian.littleEndian)
-			pfm.put(magic ~ byteWidth ~ space ~ byteHeight ~ endLine ~ ubyte(45) ~ endOfHeader ~ endLine);
-		else pfm.put(magic ~ byteWidth ~ space ~ byteHeight ~ endLine ~ endOfHeader ~ endLine);
+			pfm.put(
+				magic ~
+				byteWidth ~ space ~ byteHeight ~ endLine ~
+				ubyte(45) ~ endOfHeader ~ endLine
+				);
+		else pfm.put(
+			magic ~
+			byteWidth ~ space ~ byteHeight ~ endLine ~
+			endOfHeader ~ endLine
+			);
 
-		Color col;
-		for (int i = height - 1; i > -1; --i)
+		Color c;
+		for (int i = height - 1; i > -1; --i) //++i
 		{
 			for (int j = 0; j < width; ++j)
 			{
-				col = getPixel(j, i);
+				c = getPixel(j, i);
 				if (endianness == Endian.bigEndian)
 				{
-					pfm.append!(uint, Endian.bigEndian)(*cast(uint*)(&col.r));
-					pfm.append!(uint, Endian.bigEndian)(*cast(uint*)(&col.g));
-					pfm.append!(uint, Endian.bigEndian)(*cast(uint*)(&col.b));
+					pfm.append!(uint, Endian.bigEndian)(*cast(uint*)(&c.r));
+					pfm.append!(uint, Endian.bigEndian)(*cast(uint*)(&c.g));
+					pfm.append!(uint, Endian.bigEndian)(*cast(uint*)(&c.b));
 				}
 				else
 				{
-					pfm.append!(uint, Endian.littleEndian)(*cast(uint*)(&col.r));
-					pfm.append!(uint, Endian.littleEndian)(*cast(uint*)(&col.g));
-					pfm.append!(uint, Endian.littleEndian)(*cast(uint*)(&col.b));
+					pfm.append!(uint, Endian.littleEndian)(*cast(uint*)(&c.r));
+					pfm.append!(uint, Endian.littleEndian)(*cast(uint*)(&c.g));
+					pfm.append!(uint, Endian.littleEndian)(*cast(uint*)(&c.b));
 				}
 			}
 		}
@@ -375,7 +390,7 @@ class HDRImage
 	}
 
 	/// Write a PFM file with a given name, with Endianness "little Endian" from an array of ubyte
-	/* @safe */ void writePFMFile(string fileName, in Endian endianness = Endian.littleEndian) const
+	@safe void writePFMFile(string fileName, in Endian endianness = Endian.littleEndian) const
 	{
 		if (fileName == [])
 		{
@@ -388,7 +403,7 @@ class HDRImage
 			fileName ~= ".pfm";
 			writeln("WARNING: pfm file automatically renamed to ", fileName);
 		}
-		File file = File(fileName, "wb");
+		auto file = File(fileName, "wb");
 		file.rawWrite(writePFM(endianness));
 	}
 
@@ -401,38 +416,39 @@ class HDRImage
 			return;
 		}
 
-		ubyte[] data;
-		foreach (Color c; pixels)
-		{
-			data ~= to!ubyte(round(255 * pow(c.r, 1 / gamma)));
-			data ~= to!ubyte(round(255 * pow(c.g, 1 / gamma)));
-			data ~= to!ubyte(round(255 * pow(c.b, 1 / gamma)));
-		}
 		if (!fileName.endsWith(".png"))
 		{
 			fileName ~= ".png";
 			writeln("WARNING: file automatically renamed to ", fileName);
 		}
-		imageformats.png.write_png(fileName, width, height, data, 0);
+		auto rgb = appender!(ubyte[]);
+		foreach (Color c; pixels)
+		{
+			rgb.put(to!ubyte(round(255 * pow(c.r, 1 / gamma))));
+			rgb.put(to!ubyte(round(255 * pow(c.g, 1 / gamma))));
+			rgb.put(to!ubyte(round(255 * pow(c.b, 1 / gamma))));
+		}
+		imageformats.png.write_png(fileName, width, height, rgb.data, 0);
 	}
 
 	/// Return the average luminosity of an HDRImage
-	pure nothrow @nogc @safe float averageLuminosity(in float delta=1e-10) const
+	pure nothrow @safe float averageLuminosity(in float delta=1e-10) const
 	{
 		float lumSum = 0.0;
-        foreach (p; pixels[]) lumSum += log10(delta + p.luminosity);
+        foreach (Color p; pixels[]) lumSum += log10(delta + p.luminosity);
         return pow(10, lumSum / pixels.length);
 	}
 
 	/// Normalize each pixel of an HDRImage multiplying by the ratio: factor / luminosity
-	pure nothrow @nogc @safe void normalizeImage(in float factor, float luminosity = NaN(0x3FFFFF))
+	pure nothrow @safe void normalizeImage(in float factor, float luminosity = float.init)
 	{
-		if (luminosity.isNaN()) luminosity = averageLuminosity;
-		for (int i = 0; i < pixels.length; ++i) pixels[i] = pixels[i] * (factor / luminosity);
+		if (luminosity.isNaN) luminosity = averageLuminosity;
+		for (int i = 0; i < pixels.length; ++i)
+			pixels[i] = pixels[i] * (factor / luminosity);
 	}
 
 	/// Correct the colors of an HDRImage calling clamp on every r,b,g component in every pixel
-	pure nothrow @nogc @safe void clampImage()
+	pure nothrow @safe void clampImage()
 	{
 		for (int i = 0; i < pixels.length; ++i)
 		{
@@ -446,7 +462,7 @@ class HDRImage
 ///
 unittest
 {
-	HDRImage img = new HDRImage(7,4);
+	auto img = new HDRImage(7,4);
 
 	// validCoordinates
 	assert(img.validCoordinates(0, 0)); 
@@ -463,8 +479,8 @@ unittest
 ///
 unittest
 {
-	HDRImage img = new HDRImage(2,1);
-	Color c1 = {5.0, 10.0, 15.0}, c2 = {500.0, 1000.0, 1500.0};
+	auto img = new HDRImage(2,1);
+	auto c1 = Color(5.0, 10.0, 15.0), c2 = Color(500.0, 1000.0, 1500.0);
 	img.setPixel(0, 0, c1);
 	img.setPixel(1, 0, c2);
 		
@@ -473,7 +489,7 @@ unittest
 	// averageLuminosity
 	assert(areClose(100.0, img.averageLuminosity(0.0)));
 		
-	Color c3 = {0.5e2, 1.0e2, 1.5e2}, c4 = {0.5e4, 1.0e4, 1.5e4};
+	auto c3 = Color(0.5e2, 1.0e2, 1.5e2), c4 = Color(0.5e4, 1.0e4, 1.5e4);
 	img.normalizeImage(1000.0, 100.0);
 
 	// colorIsClose
@@ -484,14 +500,14 @@ unittest
 ///
 unittest
 {
-	HDRImage img = new HDRImage(2, 1);
+	auto img = new HDRImage(2, 1);
 		
-	Color c1 = {0.5e1, 1.0e1, 1.5e1}, c2 = {0.5e3, 1.0e3, 1.5e3};
+	auto c1 = Color(0.5e1, 1.0e1, 1.5e1), c2 = Color(0.5e3, 1.0e3, 1.5e3);
 	img.setPixel(0, 0, c1);
 	img.setPixel(1, 0, c2);
 	img.clampImage();
 		
-	foreach (pixel; img.pixels)
+	foreach (Color pixel; img.pixels)
 	{
 		// Check RGB boundaries
 		assert(pixel.r >= 0 && pixel.r <= 1);
@@ -503,7 +519,7 @@ unittest
 ///
 unittest
 {
-	HDRImage img = new HDRImage(3,2);
+	auto img = new HDRImage(3,2);
 	img.setPixel(0, 0, Color(1.0e1, 2.0e1, 3.0e1));
 	img.setPixel(1, 0, Color(4.0e1, 5.0e1, 6.0e1));
 	img.setPixel(2, 0, Color(7.0e1, 8.0e1, 9.0e1));
@@ -537,11 +553,11 @@ unittest
 ///
 unittest
 {
-	string[2] files = ["reference_le.pfm", "reference_be.pfm"];
+	auto files = ["reference_le.pfm", "reference_be.pfm"];
 
 	foreach (string fileName; files)
 	{
-		HDRImage img = new HDRImage(fileName);
+		auto img = new HDRImage(fileName);
 
 		assert(img.width == 3);
 		assert(img.height == 2);
